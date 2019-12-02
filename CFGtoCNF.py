0d2b2047a4e7e88f1abd123932ead034fb286147
mooreka21 <mooreka21@up.edu>
2019-12-02 00:36:00 -0800

    step 3 completed

--- CFGtoCNF.py
+++ CFGtoCNF.py
@@ -6,6 +6,7 @@
 
 import re
 
+
 #################################################
 # convertToString - reads input file and converting
 # to a string
@@ -27,106 +28,152 @@ def convertToString(inputfile):
 # performs conversion to CNF and writes to output file
 #################################################
 def convertToCNF(CFG, outputfile):
-    rules = []
-    language = []
     with open(outputfile, "w") as out:
-        # step 1: make new start rule
+        # STEP 1: make new start rule
         start = CFG[0]   # finding original start letter
         firstline = start + "0->" + start
         CNF = firstline + "\n" + CFG
         stage1 = CNF
-        out.write("STEP 1: \n\n" + stage1 + "\n")
+        # print updated grammar to output file
+        out.write("STEP 1: \n\n" + stage1 + "\n\n")
 
-        # step 2: remove all empty strings ------TO-DO--------
-        list = CNF.split("\n")  # split up CNF into seperate strings for each line
-        count = sum('e' in s for s in list)
+        # STEP 2: remove all empty strings ------TO-DO--------
+        mylist = CNF.split("\n")  # split up CNF into seperate strings for each line
+        count = sum('e' in s for s in mylist)
         while count > 0:
             for loop in range(count):
-                for k in range(len(list)):  # loop through each line
-                    for i in range(len(list[k])):   # loop through contents of line
-                        if list[k][i] == "e":
-                            rule = list[k][0]   # find rule that contains e
-                            for j in range(len(list)):  # search through to find the rule in contents of other rules
-                                if list[j].find(rule) != 0:    # so we don't count the rule itself
-                                    location = list[j].find(rule)
+                for k in range(len(mylist)):  # loop through each line
+                    for i in range(len(mylist[k])):   # loop through contents of line
+                        if mylist[k][i] == "e":
+                            rule = mylist[k][0]   # find rule that contains e
+                            for j in range(len(mylist)):  # search through to find the rule in contents of other rules
+                                if mylist[j].find(rule) != 0:    # so we don't count the rule itself
+                                    location = mylist[j].find(rule)
                                     if location == -1:
                                         continue
 
                                     # case: rule is not combined with any other character
-                                    if (list[j][location - 1] == ">" or list[j][location - 1] == "|") and (
-                                            list[j][location + 1] == "|" or list[j][location + 1] == "\n"):
-                                        list[j] = list[j] + "|e"  # propagating empty string
+                                    if (mylist[j][location - 1] == ">" or mylist[j][location - 1] == "|") and (
+                                            mylist[j][location + 1] == "|" or mylist[j][location + 1] == "\n"):
+                                        mylist[j] = mylist[j] + "|e"  # propagating empty string
                                         # remove original empty string from rule
-                                        if list[k][i - 1] == "|" and list[k][i] == "e":  # there is a case before e
-                                            list[k] = list[k].replace("|e", "")
-                                        elif list[k][i - 1] == ">" and list[k][i] == "e":  # e is only case for rule
-                                            list[k] = list[k].replace(list[k], "")
+                                        if mylist[k][i - 1] == "|" and mylist[k][i] == "e":  # there is a case before e
+                                            mylist[k] = mylist[k].replace("|e", "")
+                                        elif mylist[k][i - 1] == ">" and mylist[k][i] == "e":  # e is only case for rule
+                                            mylist[k] = mylist[k].replace(mylist[k], "")
 
                                     # case: rule is combined with chars
                                     # there are two chars proceeding rule
-                                    elif (list[j][location - 1] != "|" and list[j][location - 1] != ">") and (
-                                            list[j][location - 2] != "|" and list[j][location - 2] != ">"):
-                                        list[j] = list[j] + "|" + list[j][location - 2:location]
+                                    elif (mylist[j][location - 1] != "|" and mylist[j][location - 1] != ">") and (
+                                            mylist[j][location - 2] != "|" and mylist[j][location - 2] != ">"):
+                                        mylist[j] = mylist[j] + "|" + mylist[j][location - 2:location]
                                         # remove original empty string from rule
-                                        if list[k][i - 1] == "|" and list[k][i] == "e":  # there is a case before e
-                                            list[k] = list[k].replace("|e", "")
-                                        elif list[k][i - 1] == ">" and list[k][i] == "e":  # e is only case for rule
-                                            list[k] = list[k].replace(list[k], "")
+                                        if mylist[k][i - 1] == "|" and mylist[k][i] == "e":  # there is a case before e
+                                            mylist[k] = mylist[k].replace("|e", "")
+                                        elif mylist[k][i - 1] == ">" and mylist[k][i] == "e":  # e is only case for rule
+                                            mylist[k] = mylist[k].replace(mylist[k], "")
                                     # there is one char proceeding and following rule
                                     # elif (list[j][location - 1] != "|" and list[j][location - 1] != ">") and (
                                     #       list[j][location + 1] != "|" and list[j][location + 1] != "\n"):
                                     #     list[j] = list[j] + "|" + list[j][location - 1] + list[j][location + 1]
                                     # there is one char proceeding rule
-                                    elif list[j][location - 1] != "|" and list[j][location - 1] != ">":
-                                        list[j] = list[j] + "|" + list[j][location - 1]
+                                    elif mylist[j][location - 1] != "|" and mylist[j][location - 1] != ">":
+                                        mylist[j] = mylist[j] + "|" + mylist[j][location - 1]
                                     # there are two chars following rule
                                     # if (list[j][location + 1] != "|" and list[j][location + 1] != "\n") and (
                                     # list[j][location + 2] != "|" and list[j][location + 2] != "\n"):
                                     # list[j] = list[j] + "|" + list[j][location - 2:location]
-
+                # convert list back into string
                 string = ""
-                for i in range(len(list)):
-                    string = string + list[i] + "\n"
-
+                for i in range(len(mylist)):
+                    string = string + mylist[i] + "\n"
                     CNF = CNF.replace(CNF[0:], string)
-                count = sum('e' in s for s in list)
+                count = sum('e' in s for s in mylist)
 
         stage2 = CNF
+        # print updated grammar to output file
         out.write("STEP 2: \n\n" + stage2 + "\n")
-        # step 3: remove S0->S (copy S to S0)
-        # 2nd line always begins at index 6 because step1 initializes the first line with 6 chars
-        new = start + "0" + CNF[7:CNF.find("\n", 6)]
+
+        # STEP 3: remove S0->S (copy S to S0) and remove any solo rules within a rule's contents
+        new = start + "0" + CNF[7:CNF.find("\n", 6)]    # first line is initialized with 6 chars; start index of line 2
         CNF = CNF.replace(firstline, new)   # replace the first line with a copy of the original CFG's first line
 
-        # case: remove any rules that point to a solo rule (single uppercase character)
+        # remove any rules that point to a solo rule (single uppercase character)
         mylist = CNF.split("\n")  # split up CNF into separate strings for each line
+        print(mylist)
+        copy = ""
         for k in range(len(mylist)):
-            for i in range(len(mylist[k])):
-                word = re.compile("(->[A-Z]\|)|(\|[A-Z]\|)|(\|[A-Z]$)")
-                if word.match(mylist[k]):
-                    mylist[k]
+            for i in range(len(mylist[k])-1):
+                if i != 0:
+                    # first location of rule contents
+                    if mylist[k][i-1] == ">" and mylist[k][i+1] == "|" and mylist[k][i].isupper():
+                        rule = mylist[k][i]
+                        print(rule)
+                        for j in range(len(mylist)-1):
+                            if mylist[j][0] == rule:
+                                copy = mylist[j][mylist[j].find(">")+1:]
+                                print(copy)
+                        mylist[k] = mylist[k].replace(mylist[k][i], copy)
+                        print(mylist[k])
+                        print(i)
+                    # middle location of rule contents
+                    elif mylist[k][i - 1] == "|" and mylist[k][i + 1] == "|" and mylist[k][i].isupper():
+                        rule = mylist[k][i]
+                        print(rule)
+                        for j in range(len(mylist) - 1):
+                            if mylist[j][0] == rule:
+                                copy = mylist[j][mylist[j].find(">") + 1:]
+                                print(copy)
+                        mylist[k] = mylist[k].replace(mylist[k][i], copy)
+                        print(mylist[k])
+                        print(i)
+                    # last location of rule contents
+                    # elif mylist[k][i-1] == "|" and mylist[k][i+1] == "" and mylist[k][i].isupper():
+                    #     rule = mylist[k][i]
+                    #     print(rule)
+                    #     for j in range(len(mylist) - 1):
+                    #         if mylist[j][0] == rule:
+                    #             copy = mylist[j][mylist[j].find(">") + 1:]
+                    #             print(copy)
+                    #     mylist[k] = mylist[k].replace(mylist[k][i], copy)
+                    #     print(mylist[k])
+                    #     print(i)
+                    # last location of rule contents
+                    elif mylist[k][-2] == "|" and mylist[k][-1].isupper():
+                        rule = mylist[k][-1]
+                        for j in range(len(mylist)-1):
+                            if mylist[j][0] == rule:
+                                copy = mylist[j][mylist[j].find(">") + 1:]
+                        mylist[k] = mylist[k].replace(mylist[k][-1], copy)
 
+        # convert list back into string
+        string = ""
+        for i in range(len(mylist)):
+            string = string + mylist[i] + "\n"
+            CNF = CNF.replace(CNF[0:], string)
         stage3 = CNF
+        # print updated grammar to output file
         out.write("STEP 3: \n\n" + stage3 + "\n")
 
-        # check to see if any rule goes to itself
-        # make sure no components are just one upper case letter
-        # step 4: remove rules that go to three or more terms ------TO-DO--------
+        # STEP 4: remove rules that go to three or more terms ------TO-DO--------
+        for k in range(len(mylist)):
+            for i in range(len(mylist[k])):
+                if "->" and "|" not in mylist[k][i:i+3]:
+                    triple = mylist[k][i:i+3]
 
         stage4 = ""
         out.write("STEP 4: \n\n" + stage4 + "\n")
 
         # FOR STEP 5 WE MIGHT NEED REGEX TO CHECK UPPER/LOWER
-        # step 5: remove rules that have capital letters combined with lowercase letters ------TO-DO--------
+        # STEP 5: remove rules that have capital letters combined with lowercase letters ------TO-DO--------
 
         stage5 = ""
         out.write("STEP 5: \n\n" + stage5 + "\n")
-
 
 
 # running program
 my_CFG = convertToString("CFG.txt")
 convertToCNF(my_CFG, "test.txt")
 
-my_CFG = convertToString("CFG2.txt")
-convertToCNF(my_CFG, "test2.txt")
+#my_CFG = convertToString("CFG2.txt")
+#convertToCNF(my_CFG, "test2.txt")
